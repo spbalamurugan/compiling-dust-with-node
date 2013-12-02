0d1f260681248b68f1f843f79a1869261bfd8efc
spbalamurugan <spbalamuruga@gmail.com>
2013-12-02 23:07:00 +0530

    server side file

---
+++ hello.js
@@ -0,0 +1,15 @@
+var dustfs = require('dustfs');
+dustfs.dirs('../dust');
+var jsonObj = {
+				"name1":"vinayagar",
+				"exists":true,
+				"items":"shoes",
+				"names":[{"name":"reebok"},{"name":"nike"},{"name":"woodlands"}]
+};
+console.log(typeof(jsonObj));
+//console.log(JSON.stringify(jsonObj));
+console.log("is", jsonObj.exists);
+dustfs.render('hello.dust',(jsonObj),function(err,out){
+if(err) console.log('Error: '+err);
+else console.log(out);
+});
