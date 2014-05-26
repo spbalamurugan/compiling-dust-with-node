752c337ad9943e90a25e150554c4671b17175a91
spbalamurugan <spbalamuruga@gmail.com>
2014-05-26 21:16:32 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,14 +1,35 @@
 var dustfs = require('dustfs');
 dustfs.dirs('./dust');
 var jsonObj = {
-				"name1":"vinayagar",
-				"exists":true,
-				"items":"shoes",
-				"names":[{"name":"reebok"},{"name":"nike"},{"name":"woodlands"}]
+				"Users":[{"name":"Developer",
+				"experience":12,
+				"passport":true,
+				"skills":[{"name":"Problem Solving"},{"name":"Algorithms"},{"name":"enthusiasm"}]
+},
+{"name":"Manager",
+				"experience":10,
+				"passport":false,
+				"skills":[{"name":"Problem Analysis"},{"name":"communication"},{"name":"Orator"}]
+},
+{"name":"Architect",
+				"experience":12,
+				"passport":true,
+				"skills":[{"name":"Design"},{"name":"Develop"},{"name":"Research"}]
+}]
 };
+// Create obj using an object literal
+var obj = {key: 'value'};
+
+// Convert to text using JSON.stringify
+var text = JSON.stringify(obj);
+
+// Show the value of text
+console.log("stringify value = "+ text ); // {"key":"value"}
+console.log(typeof(text));
 console.log(typeof(jsonObj));
-//console.log(JSON.stringify(jsonObj));
-console.log("is", jsonObj.exists);
+console.log(JSON.stringify(jsonObj));
+console.log(typeof(jsonObj));
+console.log("Code developer skills are ", jsonObj);
 dustfs.render('hello.dust',(jsonObj),function(err,out){
 if(err) console.log('Error: '+err);
 else console.log(out);
